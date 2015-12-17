401556699ddee5b02e471551a3fa726b430de386
addrum <adamshort1994@Gmail.com>
2015-12-17 17:36:23 +0000

    initial commit

---
+++ html/src/com/main/francois/client/HtmlLauncher.java
@@ -0,0 +1,19 @@
+package com.main.francois.client;
+
+import com.badlogic.gdx.ApplicationListener;
+import com.badlogic.gdx.backends.gwt.GwtApplication;
+import com.badlogic.gdx.backends.gwt.GwtApplicationConfiguration;
+import com.main.francois.Francois;
+
+public class HtmlLauncher extends GwtApplication {
+
+        @Override
+        public GwtApplicationConfiguration getConfig () {
+                return new GwtApplicationConfiguration(480, 320);
+        }
+
+        @Override
+        public ApplicationListener getApplicationListener () {
+                return new Francois();
+        }
+}
