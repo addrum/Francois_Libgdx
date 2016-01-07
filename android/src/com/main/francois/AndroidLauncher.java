038ddccd721e6a3a352ba0cb4f262c9e21b6964d
addrum <adamshort1994@Gmail.com>
2016-01-07 16:35:51 +0000

    add relevant ad changes for android (WIP)

--- android/src/com/main/francois/AndroidLauncher.java
+++ android/src/com/main/francois/AndroidLauncher.java
@@ -1,17 +1,24 @@
 package com.main.francois;
 
 import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
+import android.widget.RelativeLayout;
+import android.widget.RelativeLayout.LayoutParams;
 
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Preferences;
 import com.badlogic.gdx.backends.android.AndroidApplication;
 import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
-import com.badlogic.gdx.files.FileHandle;
-import com.badlogic.gdx.utils.I18NBundle;
 import com.francois.main.core.ActionResolver;
 import com.francois.main.core.Francois;
 import com.francois.main.core.MainMenuScreen;
+import com.google.android.gms.ads.AdRequest;
+import com.google.android.gms.ads.AdSize;
+import com.google.android.gms.ads.AdView;
 import com.google.android.gms.common.api.ResultCallback;
 import com.google.android.gms.games.Games;
 import com.google.android.gms.games.leaderboard.LeaderboardVariant;
@@ -19,9 +26,12 @@ import com.google.android.gms.games.leaderboard.Leaderboards;
 import com.google.example.games.basegameutils.GameHelper;
 import com.google.example.games.basegameutils.GameHelper.GameHelperListener;
 
-import com.badlogic.gdx.Preferences;
-
 public class AndroidLauncher extends AndroidApplication implements GameHelperListener, ActionResolver {
+    protected AdView adView;
+    protected View gameView;
+
+    private String AD_UNIT_ID;
+
     // customs
     private GameHelper gameHelper;
     private MainMenuScreen mainMenuScreen;
@@ -43,6 +53,51 @@ public class AndroidLauncher extends AndroidApplication implements GameHelperLis
         AndroidApplicationConfiguration cfg = new AndroidApplicationConfiguration();
         cfg.useAccelerometer = false;
         cfg.useCompass = false;
+
+        // Do the stuff that initialize() would do for you
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        getWindow().clearFlags(WindowManager.LayoutParams.FLAG_FORCE_NOT_FULLSCREEN);
+
+        RelativeLayout layout = new RelativeLayout(this);
+        RelativeLayout.LayoutParams params = new RelativeLayout.LayoutParams(RelativeLayout.LayoutParams.MATCH_PARENT, RelativeLayout.LayoutParams.MATCH_PARENT);
+        layout.setLayoutParams(params);
+
+        AdView admobView = createAdView();
+        layout.addView(admobView);
+        View gameView = createGameView(cfg);
+        layout.addView(gameView);
+
+        setContentView(layout);
+        startAdvertising(admobView);
+    }
+
+    private AdView createAdView() {
+        adView = new AdView(this);
+        adView.setAdSize(AdSize.SMART_BANNER);
+        adView.setAdUnitId(AD_UNIT_ID);
+        adView.setId(R.id.adViewId); // this is an arbitrary id, allows for relative positioning in createGameView()
+        RelativeLayout.LayoutParams params = new RelativeLayout.LayoutParams(LayoutParams.MATCH_PARENT, LayoutParams.WRAP_CONTENT);
+        params.addRule(RelativeLayout.ALIGN_PARENT_TOP, RelativeLayout.TRUE);
+        params.addRule(RelativeLayout.CENTER_HORIZONTAL, RelativeLayout.TRUE);
+        adView.setLayoutParams(params);
+        adView.setBackgroundColor(Color.BLACK);
+        return adView;
+    }
+
+    private View createGameView(AndroidApplicationConfiguration cfg) {
+        gameView = initializeForView(new Francois(this), cfg);
+        RelativeLayout.LayoutParams params = new RelativeLayout.LayoutParams(LayoutParams.MATCH_PARENT, LayoutParams.WRAP_CONTENT);
+        params.addRule(RelativeLayout.ALIGN_PARENT_BOTTOM, RelativeLayout.TRUE);
+        params.addRule(RelativeLayout.CENTER_HORIZONTAL, RelativeLayout.TRUE);
+        params.addRule(RelativeLayout.BELOW, adView.getId());
+        gameView.setLayoutParams(params);
+        return gameView;
+    }
+
+    private void startAdvertising(AdView adView) {
+        AdRequest adRequest = new AdRequest.Builder().build();
+        adView.loadAd(adRequest);
     }
 
     @Override
@@ -85,7 +140,8 @@ public class AndroidLauncher extends AndroidApplication implements GameHelperLis
                         prefs.putString("highscore", Long.toString(scoreResult.getScore().getRawScore()));
                         prefs.flush();
                     }
-                } catch (Exception e) {}
+                } catch (Exception e) {
+                }
             }
         });
     }
